5cea7cbd2971305811d9442d589fdcee953010a5
manonaSamir <bena.yalla@yahoo.com>
2024-03-19 11:12:12 +0200

    add city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """Representation of city """
+    __tablename__ = "cities"
+    __table_args__ = (
+        {'mysql_default_charset': 'latin1'})
+    if models.storage_t == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        state_id = ""
+        name = ""
