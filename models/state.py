7684b1fc8b87f888e40d5a67e69b2504bfa7fe39
manonaSamir <bena.yalla@yahoo.com>
2024-03-19 11:10:35 +0200

    add state class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class """
+    __tablename__ = "states"
+    __table_args__ = (
+        {'mysql_default_charset': 'latin1'})
+    if models.storage_t == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            cities_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
