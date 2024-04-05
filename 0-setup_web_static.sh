1ee76ed8c6c1c745044fb6314044e6ff4e548d89
manonaSamir <bena.yalla@yahoo.com>
2024-04-05 23:14:18 +0200

    d

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,13 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-#sudo chown $USER:$USER /var/www/html/index.html
+sudo chown ubuntu: /var/www/html/index.html
 sudo touch index.html /var/www/html/
 echo 'Holberton School' > /var/www/html/index.html
 sudo mkdir /var/www/error
-#sudo chown  $USER:$USER /var/www/error/
+sudo chown  ubuntu: /var/www/error/
 echo "Ceci n'est pas une page" > /var/www/error/404.html
-#sudo chown  $USER:$USER /etc/nginx/sites-available/
+sudo chown  ubuntu: /etc/nginx/sites-available/
 sed -i '/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlw\u4? permanent; }' /etc/nginx/sites-available/default
 sed -i '/server_name _/a error_page 404 /404.html; location = /404.html {root /var/www/error/; internal; }' /etc/nginx/sites-available/default
 # in the server 1 also 2 this down
@@ -17,7 +17,7 @@ sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/site
 # sets up your web servers for the deployment of web_static. It must in airbnb
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-#sudo chown $USER:$USER /data/web_static/releases/test/
+sudo chown ubuntu: /data/web_static/releases/test/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 #el comando de abajo permite copiar el contenido del directorio inicial al otro current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
