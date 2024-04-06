661f10f10967c2c8c87b126b8365662993fd9c33
manonaSamir <bena.yalla@yahoo.com>
2024-04-06 12:15:50 +0200

    sss

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -16,7 +16,7 @@ def do_pack():
               format(time.strftime("%Y%m%d%H%M%S")))
         return ("versions/web_static_{}.tgz".format(time.
                                                     strftime("%Y%m%d%H%M%S")))
-    except:
+    except SpecificException as e:
         return None
 
 
@@ -34,11 +34,13 @@ def do_deploy(archive_path):
         run("rm /tmp/{}".format(file))
         run("mv {}/web_static/* {}/".format(folder, folder))
         run("rm -rf {}/web_static".format(folder))
+        run("echo '<html><head></head><body>New version</body></html>' > \
+            {}/my_index.html".format(folder))
         run('rm -rf /data/web_static/current')
         run("ln -s {} /data/web_static/current".format(folder))
         print("Deployment done")
         return True
-    except:
+    except SpecificException as e:
         return False
 
 
