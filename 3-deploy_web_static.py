88a7632436f91d38343ecd90840f766a53a34e93
manonaSamir <bena.yalla@yahoo.com>
2024-04-06 13:01:01 +0200

    ff

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -45,8 +45,10 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+    """ creates and distributes an archive to your web servers
+    """
+    new_archive_path = do_pack()
+    if exists(new_archive_path) is False:
         return False
-    return do_deploy(archive_path)
+    result = do_deploy(new_archive_path)
+    return result
