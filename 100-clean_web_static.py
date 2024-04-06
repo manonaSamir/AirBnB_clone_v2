f3889dce271695d1575a4c7e41fabf3df26c96c3
manonaSamir <bena.yalla@yahoo.com>
2024-04-06 14:55:12 +0200

    c

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Function that deploys """
-from fabric.api import *
 
+from fabric.api import *
 
 env.hosts = ['54.85.153.236', '54.160.89.208']
 env.user = "ubuntu"
@@ -9,7 +9,6 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     """ CLEANS """
-
     number = int(number)
 
     if number == 0:
@@ -19,4 +18,34 @@ def do_clean(number=0):
 
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    with cd(path):
+        sudo('ls -t | tail -n +{} | xargs rm -rf'.format(number))
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        do_clean(sys.argv[1])
+    else:
+        print("Usage: python script_name.py number")
+
+
+# from fabric.api import *
+
+
+# env.hosts = ['54.85.153.236', '54.160.89.208']
+# env.user = "ubuntu"
+
+
+# def do_clean(number=0):
+#     """ CLEANS """
+
+#     number = int(number)
+
+#     if number == 0:
+#         number = 2
+#     else:
+#         number += 1
+
+#     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+#     path = '/data/web_static/releases'
+#     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
