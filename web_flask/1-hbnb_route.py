b2ab497024f2b2c8fc5aa8f778ad0e45f79210be
manonaSamir <bena.yalla@yahoo.com>
2024-04-19 13:03:55 +0200

    add shebang

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /hbnb: Displays 'HBNB!'
+"""
+
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+"""
+
+
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
