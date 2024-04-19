4c16b34c3c83e38dfa113d6d989f3c6bb7e5019d
manonaSamir <bena.yalla@yahoo.com>
2024-04-19 23:46:29 +0200

    remove port

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """Starts a Flask web application.
 """
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
@@ -24,4 +24,4 @@ def teardown(exc):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
