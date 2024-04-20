b9d2f0397ca1abe9c5baccb73d8ae82e0fafbc72
manonaSamir <bena.yalla@yahoo.com>
2024-04-20 21:14:49 +0200

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,8 +13,9 @@ def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+    #return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
