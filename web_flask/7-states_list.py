619e668c83a6542ee568eeef21516db4d2ed527e
manonaSamir <bena.yalla@yahoo.com>
2024-04-21 22:36:32 +0200

    change list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,13 +9,29 @@ from models import storage
 app = Flask(__name__)
 
 
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+
+
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
     states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 @app.teardown_appcontext
