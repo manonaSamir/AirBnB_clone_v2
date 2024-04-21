1b697b48385508bb9352f4b012fe25ef50ed38da
manonaSamir <bena.yalla@yahoo.com>
2024-04-21 11:50:18 +0200

    remove comment

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,6 @@ def states_list():
     """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
-    #return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
