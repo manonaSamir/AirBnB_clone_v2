8da2169cfbd44f8bb82470be242228b44c2ef237
menna samir <36510244+manonaSamir@users.noreply.github.com>
2024-05-15 22:24:00 +0300

    Update 0-hello_route.py
    add new route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,5 +15,10 @@ app = Flask(__name__)
 def home():
     return ("Hello HBNB!")
 
+@app.route("/airbnb-onepage", strict_slashes=False)
+def home():
+    return ("Hello HBNB!")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
